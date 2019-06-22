b8c8eb4f8f140705aadf0f671855f714b91f946d
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-22 01:28:32 +0000

    implement selecting dates to update database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -72,9 +72,33 @@ $(document).ready(function () {
 
 
   // for now, this only toggles ALL buttons - need to specify it later
-  $('#row-submit-btn').css('visibility', 'visible');
+  $('.btn-go').css('visibility', 'visible');
   // window.location.href = `/events${window.location.pathname.substring(7)}`;
   // window.stop();
+
+  $('.btn-go').on('click', function(event) {
+    let email = $(this).parent().parent().attr("id");
+    let selectedArray = $(this).parent().parent().find(".selected");
+    let event_url = window.location.pathname.substring(8);
+    let postObj = {
+      dates: {
+        'selected_date_1': null,
+        'selected_date_2': null,
+        'selected_date_3': null,
+        'selected_date_4': null,
+        'selected_date_5': null
+      },
+      email,
+      event_url
+    };
+    selectedArray.each(function() {
+      postObj['dates'][$(this).attr("id")] = '1111-11-11';
+    });
+    //console.log(postObj);
+    $.post(event_url, postObj, function(data){
+      console.log(data, postObj);
+    });
+  });
 
   $('.table-option').on('click', function (event) {
 
